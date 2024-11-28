b7cd4002315b2add59fb1de3ab22ee917ee0476d
dionisio202 <solisedison@outlook.com>
2024-11-27 20:05:25 -0500

    fixed routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from "./templates/Profile";
-
+import Login from "./auth/SignInForm";
+import ForgotPassword from "./auth/ForgotPassword"; // Importa tus componentes
+import Register from "./auth/Register";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Profile />} />
+        <Route path="/profile" element={<Profile />} />
+           {/* Ruta de Inicio */}
+      <Route path="/" element={<Login />} />
+      {/* Ruta de Recuperar Contraseña */}
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      {/* Puedes agregar más rutas aquí */}
+      <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );

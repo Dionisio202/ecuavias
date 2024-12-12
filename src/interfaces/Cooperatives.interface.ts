d7cf8876ccad8028320403d9785e779a4b840f7c
Matt <mateo19dg@gmail.com>
2024-12-11 22:19:28 -0500

    TASK: CRUD for Cooperatives finished.

--- src/interfaces/Cooperatives.interface.ts
+++ src/interfaces/Cooperatives.interface.ts
@@ -1,15 +1,15 @@
 
 export interface Cooperative {
-    id_coop: string,
+    id_coop?: string,
     name: string,
     ruc: string,
     address: string,
     website_url: string,
     logo_url: string,
     buses_qty: number,
-    status: string,
-    created_by: string,
-    date_time_create: string,
-    updated_by: string,
-    date_time_update: string
+    status?: string,
+    created_by?: string,
+    date_time_create?: string,
+    updated_by?: string,
+    date_time_update?: string
 }
